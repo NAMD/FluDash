e6cb5f3a28087f138b4bea5d564527d71364714e
Flavio C. Coelho <fccoelho@gmail.com>
2015-09-26 16:00:36 -0300

    Started the series view

--- FluDash/urls.py
+++ FluDash/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from sinan.views import SeriesView
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^series/', SeriesView.as_view(), name='series'),
 ]
--- sinan/views.py
+++ sinan/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.views.generic import View
+from django.http import HttpResponse
+from bokeh.plotting import figure
+from bokeh.resources import CDN
+from bokeh.embed import components, Resources
+from bokeh.models import ColumnDataSource
+import pandas as pd
+import psycopg2
+
+
+conn = psycopg2.connect(database='influenza', user='flu', password='influenza', host='localhost')
 
 # Create your views here.
+
+class SeriesView(View):
+    def get(self, request):
+        year = request.GET['year']
+        city = request.get['geocode']
+
+        sql = 'select * from public."INFLUD{}-utf8"'.format(year)
+        try:
+            df  = pd.read_sql_query(sql=sql, con=conn, index_col='id', parse_dates=True)
+        except Exception as e:
+            raise e
+
+
