793d8eb6854f39bbbd8961c28249dda36af2925c
Flavio C. Coelho <fccoelho@gmail.com>
2015-09-27 10:58:59 -0300

    Started the database introspection code

--- sinan/models.py
+++ sinan/models.py
@@ -1,3 +1,26 @@
-from django.db import models
+"""
+In this model, instead of setting up datamodels from scratch,
+we will "reflect" an existing model using SQLAlchemy's Automap feature
+"""
 
-# Create your models here.
+from django.db import models
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, MetaData
+
+
+
+def get_base(connection_string, tables=None):
+    engine = create_engine(connection_string)
+    if tables is not None:  # reflecting only specified tables
+        metadata = MetaData()
+        metadata.reflect(engine, only=tables)
+        Base = automap_base(metadata=metadata)
+        Base.prepare()
+    else:
+        Base = automap_base()
+        Base.prepare(engine, reflect=True)
+
+    return Base
+
+
